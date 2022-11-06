e4be5f2a3e30fa0dc6239c8d8f43d986963856b3
Beyzagül Demir <85847630+beyzaguld@users.noreply.github.com>
2022-11-06 20:12:39 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,16 @@ app.use("/api/auth", authRoutes);
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
 
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
   });
 }
