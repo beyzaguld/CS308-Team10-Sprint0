63cddf04a89a16a257aa375df7c3a0d62afd83d7
Beyzagül Demir <85847630+beyzaguld@users.noreply.github.com>
2022-11-06 20:08:11 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,8 @@ const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
